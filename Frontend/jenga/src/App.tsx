81fe1173cebab829dfc7a7dd5081898a5cf05b6f
Jonas Immanuel Lehr <privat@jonas-lehr.de>
2025-09-24 01:25:05 +0200

    initial FE commit

---
+++ Frontend/jenga/src/App.tsx
@@ -0,0 +1,21 @@
+import { AppBar, Card, Toolbar } from '@suid/material';
+import type { Component } from 'solid-js';
+
+
+
+const App: Component = () => {
+  return (
+    <>
+    <AppBar>
+      <Toolbar>
+        Jenga
+      </Toolbar>
+    </AppBar>
+      <Card>
+
+      </Card>
+    </>
+  );
+};
+
+export default App;
