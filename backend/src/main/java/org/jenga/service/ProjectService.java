beaa3a0ed198c322a69336b47fe501fdf1272a8f
khde <gaha.1@gmx.de>
2025-10-07 22:19:44 +0200

    Make project service methods transactional

--- backend/src/main/java/org/jenga/service/ProjectService.java
+++ backend/src/main/java/org/jenga/service/ProjectService.java
@@ -5,6 +5,7 @@ import org.jenga.model.Project;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.NotFoundException;
 import java.util.List;
 
@@ -12,8 +13,10 @@ import java.util.List;
 public class ProjectService {
     @Inject
     ProjectRepository projectRepository;
-
+    
+    @Transactional
     public void create(Project project) {
+        projectRepository.persist(project);
     }
 
     public List<Project> findAll() {
@@ -24,10 +27,18 @@ public class ProjectService {
         return projectRepository.findByIdOptional(id)
             .orElseThrow(() -> new NotFoundException("Project not found"));
     }
-
+    
+    @Transactional
     public void update(Long id, Project project) {
+        Project existingProject = projectRepository.findByIdOptional(id)
+                .orElseThrow(() -> new NotFoundException("Project not found"));
+        existingProject.name = project.name;
+        existingProject.description = project.description;
+        projectRepository.persist(existingProject);
     }
-
+    
+    @Transactional
     public void delete(Long id) {
+        projectRepository.deleteById(id);
     }
 }
