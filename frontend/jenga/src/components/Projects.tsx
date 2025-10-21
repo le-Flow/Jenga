b0df6f5fe5e010183ceebec54d25fc9a251e4ced
Jonas Immanuel Lehr <privat@jonas-lehr.de>
2025-10-22 00:25:35 +0200

    add newProjectDialog

--- frontend/jenga/src/components/Projects.tsx
+++ frontend/jenga/src/components/Projects.tsx
@@ -1,12 +1,26 @@
-import { Button, Card, CardActions, CardContent, CardHeader, List, ListItem, ListItemButton, ListItemText } from "@suid/material"
+import { Button, Card, CardActions, CardContent, CardHeader, Dialog, DialogActions, DialogContent, DialogTitle, List, ListItem, ListItemButton, ListItemText, Stack, TextField } from "@suid/material"
 import { ProjectContext } from "../provider/ProjectProvider"
-import { For, useContext } from "solid-js"
+import { createSignal, For, useContext } from "solid-js"
 
 
+const NewProject = () => {
+    return (
+        <></>
+    )
+}
 
 export const Projects = () => {
 
+    const [open, setOpen] = createSignal(false)
+
+    const [name, setName] = createSignal("")
+    const [desc, setDesc] = createSignal("")
+
     const projectCtx = useContext(ProjectContext)
+
+    const onCreate = () => {
+
+    }
 
     return (
         <>
@@ -33,11 +47,32 @@ export const Projects = () => {
                     </List>
                 </CardContent>
                 <CardActions>
-                    <Button onClick={() => { }}>
-                        create
+                    <Button onClick={() => { setOpen(true) }}>
+                        NEW
                     </Button>
                 </CardActions>
             </Card>
+            <Dialog open={open()}>
+                <DialogTitle>New Project</DialogTitle>
+                <DialogContent>
+                    <Stack spacing={1}>
+                        <TextField name="name" label="name" value={name()} onChange={(_, value) => { setName(value) }}></TextField>
+                        <TextField name="description" label="description" value={desc()} onChange={(_, value) => { setDesc(value) }} multiline></TextField>
+                    </Stack>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => { setOpen(false)}}>
+                        cancel
+                    </Button>
+                    <Button onClick={() => { 
+
+                        setOpen(false)
+                    }}>
+                        create
+                    </Button>
+                </DialogActions>
+
+            </Dialog>
         </>
     )
 }
