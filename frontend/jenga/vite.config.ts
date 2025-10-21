8ecf7e112a5eaad5b45cc0333003fe2488fdf7a8
Jonas Immanuel Lehr <privat@jonas-lehr.de>
2025-10-21 17:26:39 +0200

    use suidPlugin

--- frontend/jenga/vite.config.ts
+++ frontend/jenga/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import suidPlugin from '@suid/vite-plugin';
 import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  plugins: [devtools(), solidPlugin()],
+  plugins: [devtools(), solidPlugin(), suidPlugin()],
   server: {
     port: 3000,
   },
