04d68fe52e5482c18c32f54bc5cf4ef587f6f19b
Jonas Immanuel Lehr <privat@jonas-lehr.de>
2025-10-22 21:18:10 +0200

    ProjectProvider provides tickets and selectedProject

--- frontend/jenga/src/provider/ProjectProvider.tsx
+++ frontend/jenga/src/provider/ProjectProvider.tsx
@@ -1,8 +1,14 @@
-import { createContext, Resource, createResource, JSXElement } from "solid-js"
-import { ProjectDTO, ProjectResourceService } from "../api"
+import { createContext, Resource, createResource, JSXElement, createEffect, Accessor, Setter, createSignal } from "solid-js"
+import { ProjectDTO, ProjectResourceService, TicketDTO, TicketResourceService } from "../api"
 
 type ProjectContextType = {
     projects: Resource<ProjectDTO[]>
+
+    selectedProject: Accessor<ProjectDTO>
+    setSelectedProject: Setter<ProjectDTO>
+
+    tickets: Resource<TicketDTO[]>
+    setTickets: Setter<TicketDTO[]>
 }
 
 export const ProjectContext = createContext<ProjectContextType>()
@@ -15,8 +21,18 @@ export const ProjectProvider = (props: ProviderProps) => {
 
     const [projects] = createResource(async () => await ProjectResourceService.getApiProjects())
 
+    const [selectedProject, setSelectedProject] = createSignal<ProjectDTO>()
+    
+    const [tickets, {mutate}] = createResource(selectedProject, async (q)=> await TicketResourceService.getApiProjectsTickets(q.identifier))
+
+    createEffect(()=> console.log(tickets()))
+
     const value = {
-        projects: projects
+        projects: projects,
+        selectedProject: selectedProject,
+        setSelectedProject: setSelectedProject,
+        tickets: tickets,
+        setTickets: mutate
     }
 
     return (
