7f7e04831af030414958cbf9ebd06b999e2a6fc4
Jonas Immanuel Lehr <privat@jonas-lehr.de>
2025-10-27 21:54:29 +0100

    basic profile information

--- frontend/jenga/src/pages/Profile.tsx
+++ frontend/jenga/src/pages/Profile.tsx
@@ -1,10 +1,18 @@
-import { Card, CardContent, CardHeader } from "@suid/material"
+import { Avatar, Card, CardContent, CardHeader } from "@suid/material"
+import { useContext } from "solid-js"
+import { UserContext } from "../provider/UserProvider"
 
 export const Profile = () => {
+    const uCtx = useContext(UserContext)
+
     return (
         <Card>
             <CardHeader title="Profile"></CardHeader>
-            <CardContent></CardContent>
+            <CardContent>
+                <Avatar></Avatar>
+                <div>username: {uCtx?.user()?.username}</div>
+                <div>email: {uCtx?.user()?.email}</div>
+            </CardContent>
         </Card>
     )
 }
