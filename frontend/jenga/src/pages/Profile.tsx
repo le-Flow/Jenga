c4f0577fa5206155e954edd773d18c0ef06aa973
Jonas Immanuel Lehr <privat@jonas-lehr.de>
2025-10-29 22:37:52 +0100

    dont show info when logged out

--- frontend/jenga/src/pages/Profile.tsx
+++ frontend/jenga/src/pages/Profile.tsx
@@ -1,17 +1,21 @@
 import { Avatar, Card, CardContent, CardHeader } from "@suid/material"
-import { useContext } from "solid-js"
+import { Show, useContext } from "solid-js"
 import { UserContext } from "../provider/UserProvider"
+import { AuthContext } from "../provider/AuthProvider"
 
 export const Profile = () => {
+    const aCtx = useContext(AuthContext)
     const uCtx = useContext(UserContext)
 
     return (
         <Card>
             <CardHeader title="Profile"></CardHeader>
             <CardContent>
-                <Avatar></Avatar>
-                <div>username: {uCtx?.user()?.username}</div>
-                <div>email: {uCtx?.user()?.email}</div>
+                <Show when={aCtx?.isLoggedIn()} fallback={<div>Please <b>login</b> to view profile info!</div>}>
+                    <Avatar></Avatar>
+                    <div>username: {uCtx?.user()?.username}</div>
+                    <div>email: {uCtx?.user()?.email}</div>
+                </Show>
             </CardContent>
         </Card>
     )
