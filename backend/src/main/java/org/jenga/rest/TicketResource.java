471e664efcd32c3b71fd49a0fb72a36634944d94
khde <gaha.1@gmx.de>
2025-10-31 13:51:09 +0100

    Refactor ticket endpoints

--- backend/src/main/java/org/jenga/rest/TicketResource.java
+++ backend/src/main/java/org/jenga/rest/TicketResource.java
@@ -14,7 +14,7 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import java.util.List;
 
-@Path("/api/projects/{projectId}/tickets")
+@Path("/api/tickets")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class TicketResource {
@@ -23,155 +23,149 @@ public class TicketResource {
     TicketService ticketService;
 
     @POST
+    @Path("/{projectId}")
     public TicketResponseDTO createTicket(@PathParam("projectId") String projectId, TicketRequestDTO ticketRequestDTO) {
         return ticketService.create(projectId, ticketRequestDTO);
     }
 
     @GET
+    @Path("/{projectId}/all")
     public List<TicketResponseDTO> getAllTickets(@PathParam("projectId") String projectId) {
         return ticketService.findAll(projectId);
     }
 
     @GET
     @Path("/{ticketId}")
-    public TicketResponseDTO getTicketById(@PathParam("projectId") String projectId, @PathParam("ticketId") Long ticketId) {
-        return ticketService.findById(projectId, ticketId);
+    public TicketResponseDTO getTicketById(@PathParam("ticketId") Long ticketId) {
+        return ticketService.findById(ticketId);
     }
 
     @GET
-    @Path("/nr/{ticketNumber}")
+    @Path("/{projectId}/{ticketNumber}")
     public TicketResponseDTO getTicketByNumber(@PathParam("projectId") String projectId, @PathParam("ticketNumber") Long ticketNumber) {
         return ticketService.findByTicketNumber(projectId, ticketNumber);
     }
 
     @PUT
     @Path("/{ticketId}")
-    public TicketResponseDTO updateTicket(@PathParam("projectId") String projectId, @PathParam("ticketId") Long ticketId, TicketRequestDTO ticketDTO) {
-        return ticketService.update(projectId, ticketId, ticketDTO);
+    public TicketResponseDTO updateTicket(@PathParam("ticketId") Long ticketId, TicketRequestDTO ticketDTO) {
+        return ticketService.update(ticketId, ticketDTO);
     }
 
     @DELETE
     @Path("/{ticketId}")
-    public Response deleteTicket(@PathParam("projectId") String projectId, @PathParam("ticketId") Long ticketId) {
-        ticketService.delete(projectId, ticketId);
+    public Response deleteTicket(@PathParam("ticketId") Long ticketId) {
+        ticketService.delete(ticketId);
         return Response.noContent().build();
     }
 
     @POST
     @Path("/{ticketId}/duplicate")
-    public TicketResponseDTO duplicateTicket(@PathParam("projectId") String projectId, @PathParam("ticketId") Long ticketId) {
-        return ticketService.duplicateTicket(projectId, ticketId);
+    public TicketResponseDTO duplicateTicket(@PathParam("ticketId") Long ticketId) {
+        return ticketService.duplicateTicket(ticketId);
     }
 
     @PUT
     @Path("/{ticketId}/assign")
-    public Response assignTicket(@PathParam("projectId") String projectId, @PathParam("ticketId") Long ticketId, @QueryParam("username") String username) {
-        ticketService.assignTicket(projectId, ticketId, username);
+    public Response assignTicket(@PathParam("ticketId") Long ticketId, @QueryParam("username") String username) {
+        ticketService.assignTicket(ticketId, username);
         return Response.status(Response.Status.NO_CONTENT).build();
     }
 
     @PUT
     @Path("/{ticketId}/unassign")
-    public Response unassignTicket(@PathParam("projectId") String projectId, @PathParam("ticketId") Long ticketId) {
-        ticketService.unassignTicket(projectId, ticketId);
+    public Response unassignTicket(@PathParam("ticketId") Long ticketId) {
+        ticketService.unassignTicket(ticketId);
         return Response.status(Response.Status.NO_CONTENT).build();
     }
 
     @POST
     @Path("/{ticketId}/comments")
-    public CommentResponseDTO createComment(@PathParam("projectId") String projectId, @PathParam("ticketId") Long ticketId, CommentRequestDTO commentDTO) {
-        return ticketService.createComment(projectId, ticketId, commentDTO);
+    public CommentResponseDTO createComment(@PathParam("ticketId") Long ticketId, CommentRequestDTO commentDTO) {
+        return ticketService.createComment(ticketId, commentDTO);
     }
 
     @GET
     @Path("/{ticketId}/comments")
-    public List<CommentResponseDTO> getAllComments(@PathParam("projectId") String projectId, @PathParam("ticketId") Long ticketId) {
-        return ticketService.getAllComments(projectId, ticketId);
+    public List<CommentResponseDTO> getAllComments(@PathParam("ticketId") Long ticketId) {
+        return ticketService.getAllComments(ticketId);
     }
 
     @DELETE
     @Path("/{ticketId}/comments/{commentId}")
-    public Response deleteComment(@PathParam("projectId") String projectId, @PathParam("ticketId") Long ticketId, @PathParam("commentId") Long commentId) {
-        ticketService.deleteComment(projectId, ticketId, commentId);
+    public Response deleteComment(@PathParam("ticketId") Long ticketId, @PathParam("commentId") Long commentId) {
+        ticketService.deleteComment(ticketId, commentId);
         return Response.noContent().build();
     }
 
     @POST
     @Path("/{ticketId}/acceptance-criteria")
     public AcceptanceCriteriaResponseDTO addAcceptanceCriteria(
-            @PathParam("projectId") String projectId,
             @PathParam("ticketId") Long ticketId,
             AcceptanceCriteriaRequestDTO request) {
-        AcceptanceCriteriaResponseDTO response = ticketService.addAcceptanceCriteria(projectId, ticketId, request);
+        AcceptanceCriteriaResponseDTO response = ticketService.addAcceptanceCriteria(ticketId, request);
         return response;
     }
 
     @GET
     @Path("/{ticketId}/acceptance-criteria")
     public List<AcceptanceCriteriaResponseDTO> getAllAcceptanceCriteria(
-            @PathParam("projectId") String projectId,
             @PathParam("ticketId") Long ticketId) {
-        List<AcceptanceCriteriaResponseDTO> criteriaList = ticketService.getAllAcceptanceCriteria(projectId, ticketId);
+        List<AcceptanceCriteriaResponseDTO> criteriaList = ticketService.getAllAcceptanceCriteria(ticketId);
         return criteriaList;
     }
 
     @PUT
     @Path("/{ticketId}/acceptance-criteria/{criteriaId}")
     public AcceptanceCriteriaResponseDTO updateAcceptanceCriteria(
-            @PathParam("projectId") String projectId,
             @PathParam("ticketId") Long ticketId,
             @PathParam("criteriaId") Long criteriaId,
             AcceptanceCriteriaRequestDTO request) {
-        return ticketService.updateAcceptanceCriteria(projectId, ticketId, criteriaId, request);
+        return ticketService.updateAcceptanceCriteria(ticketId, criteriaId, request);
     }
 
     @DELETE
     @Path("/{ticketId}/acceptance-criteria/{criteriaId}")
     public Response deleteAcceptanceCriteria(
-            @PathParam("projectId") String projectId,
             @PathParam("ticketId") Long ticketId,
             @PathParam("criteriaId") Long criteriaId) {
-        ticketService.deleteAcceptanceCriteria(projectId, ticketId, criteriaId);
+        ticketService.deleteAcceptanceCriteria(ticketId, criteriaId);
         return Response.noContent().build();
     }
 
     @PUT
     @Path("/{ticketId}/related/{relatedTicketId}")
     public Response AddRelatedTicket(
-            @PathParam("projectId") String projectId,
             @PathParam("ticketId") Long ticketId,
             @PathParam("relatedTicketId") Long relatedTicketId) {
-        ticketService.addRelatedTicket(projectId, ticketId, relatedTicketId);
+        ticketService.addRelatedTicket(ticketId, relatedTicketId);
         return Response.status(Response.Status.NO_CONTENT).build();
     }
 
     @DELETE
     @Path("/{ticketId}/related/{relatedTicketId}")
     public Response removeRelatedTicket(
-            @PathParam("projectId") String projectId,
             @PathParam("ticketId") Long ticketId,
             @PathParam("relatedTicketId") Long relatedTicketId) {
-        ticketService.removeRelatedTicket(projectId, ticketId, relatedTicketId);
+        ticketService.removeRelatedTicket(ticketId, relatedTicketId);
         return Response.status(Response.Status.NO_CONTENT).build();
     }
 
     @PUT
     @Path("/{ticketId}/block/{blockedTicketId}")
     public Response addBlockingTicket(
-            @PathParam("projectId") String projectId,
             @PathParam("ticketId") Long ticketId,
             @PathParam("blockedTicketId") Long blockedTicketId) {
-        ticketService.addBlockingTicket(projectId, ticketId, blockedTicketId);
+        ticketService.addBlockingTicket(ticketId, blockedTicketId);
         return Response.status(Response.Status.NO_CONTENT).build();
     }
 
     @DELETE
     @Path("/{ticketId}/block/{blockedTicketId}")
     public Response removeBlockingTicket(
-            @PathParam("projectId") String projectId,
             @PathParam("ticketId") Long ticketId,
             @PathParam("blockedTicketId") Long blockedTicketId) {
-        ticketService.removeBlockingTicket(projectId, ticketId, blockedTicketId);
+        ticketService.removeBlockingTicket(ticketId, blockedTicketId);
         return Response.status(Response.Status.NO_CONTENT).build();
     }
 }
