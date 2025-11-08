caed512ce205c78854b1f653a80b7f42591c9cdf
khde <gaha.1@gmx.de>
2025-11-08 17:36:30 +0100

    Restructure ticket search payload

--- backend/src/main/java/org/jenga/dto/TicketSearchDTO.java
+++ backend/src/main/java/org/jenga/dto/TicketSearchDTO.java
@@ -10,12 +10,17 @@ import lombok.Data;
 
 @Data
 public class TicketSearchDTO{
-    private String query;
     private String projectId;
-    private List<TicketPriority> priority;
-    private List<TicketSize> size;
-    private List<TicketStatus> status;
-    private List<String> reporter;
-    private List<String> assignee;
-    private List<String> labels;
+    private String query;
+    private Filter filter;
+
+    @Data
+    public static class Filter {
+        private List<TicketPriority> priority;
+        private List<TicketSize> size;
+        private List<TicketStatus> status;
+        private List<String> reporter;
+        private List<String> assignee;
+        private List<String> labels;
+    }
 }
