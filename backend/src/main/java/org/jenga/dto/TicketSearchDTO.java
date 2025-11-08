153ffe094d93a953b54126c20fe809235b6799d0
khde <gaha.1@gmx.de>
2025-11-08 15:02:51 +0100

    Basic ticket search

---
+++ backend/src/main/java/org/jenga/dto/TicketSearchDTO.java
@@ -0,0 +1,18 @@
+package org.jenga.dto;
+
+import java.util.List;
+
+import org.jenga.model.TicketPriority;
+import org.jenga.model.TicketSize;
+import org.jenga.model.TicketStatus;
+
+import lombok.Data;
+
+@Data
+public class TicketSearchDTO{
+    private String query;
+    private String projectId;
+    private List<TicketPriority> priority;
+    private List<TicketSize> size;
+    private List<TicketStatus> status;
+}
