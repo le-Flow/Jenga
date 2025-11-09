5bf79ea1ada505e91a7872e014ad27809adb8524
Jonas Immanuel Lehr <privat@jonas-lehr.de>
2025-11-09 23:41:30 +0100

    basic github import (unstyled, does not refetch tickets yet)

--- frontend/jenga/src/pages/Home.tsx
+++ frontend/jenga/src/pages/Home.tsx
@@ -1,12 +1,16 @@
-import { Card, CardContent, CardHeader } from "@suid/material"
+import { Card, CardContent, CardHeader, Stack } from "@suid/material"
 import { Projects } from "../components/Projects"
+import { Filedrop } from "../components/Filedrop";
 
 export const Home = () => {
     return (
         <Card>
             <CardHeader title="Home"></CardHeader>
             <CardContent>
-                <Projects></Projects>
+                <Stack spacing={2}>
+                    <Projects></Projects>
+                    <Filedrop></Filedrop>
+                </Stack>
             </CardContent>
         </Card>
     )
