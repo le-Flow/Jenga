7d5dc4be7a3282fbd204b59973c41db4b9877b54
khde <gaha.1@gmx.de>
2026-02-15 20:47:25 +0100

    Signin/Singup validation with hibernate

--- backend/src/main/java/org/jenga/dto/RegisterRequestDTO.java
+++ backend/src/main/java/org/jenga/dto/RegisterRequestDTO.java
@@ -1,11 +1,20 @@
 package org.jenga.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
 import lombok.Data;
 
 @Data
 public class RegisterRequestDTO {
+    @NotBlank
+    @Email
     private String email;
+
+    @NotBlank
     private String username;
+
     private String displayName;
+
+    @NotBlank
     private String password;
 }
