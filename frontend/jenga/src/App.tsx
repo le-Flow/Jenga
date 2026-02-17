e44fdde1ca98f9f7d7077d68f7ea0ff3879157c9
Jonas Immanuel Lehr <privat@jonas-lehr.de>
2026-02-17 18:05:44 +0100

    LayoutProvider

--- frontend/jenga/src/App.tsx
+++ frontend/jenga/src/App.tsx
@@ -9,6 +9,7 @@ import { Sidebar } from './components/Sidebar';
 import { Menu } from '@suid/icons-material';
 import { Footer } from './components/Footer';
 import { useLocation } from '@solidjs/router';
+import { LayoutProvider } from './provider/LayoutProvider';
 
 const theme = createTheme()
 
@@ -31,34 +32,36 @@ const App = (props: AppProps) => {
     <>
       <CssBaseline />
       <ThemeProvider theme={theme}>
-        <AuthProvider>
-          <UserProvider>
-            <ProjectProvider>
-              <AppBar position="static">
-                <Toolbar>
-                  <IconButton onClick={() => { setOpen(prev => !prev) }}>
-                    <Menu></Menu>
-                  </IconButton>
-                  <img src={logo} style={{ "height": "2vw", "width": "auto" }}></img>
-                  <Box marginLeft={"auto"}>
-                    <Auth></Auth>
+        <LayoutProvider>
+          <AuthProvider>
+            <UserProvider>
+              <ProjectProvider>
+                <AppBar position="static">
+                  <Toolbar>
+                    <IconButton onClick={() => { setOpen(prev => !prev) }}>
+                      <Menu></Menu>
+                    </IconButton>
+                    <img src={logo} style={{ "height": "2vw", "width": "auto" }}></img>
+                    <Box marginLeft={"auto"}>
+                      <Auth></Auth>
+                    </Box>
+                  </Toolbar>
+                </AppBar>
+                <Stack direction="row">
+                  <Show when={open()}>
+                    <Card sx={{ "height": "100vh", "width": "10vw" }}>
+                      <Sidebar />
+                    </Card>
+                  </Show>
+                  <Box flex={1}>
+                    {props.children}
                   </Box>
-                </Toolbar>
-              </AppBar>
-              <Stack direction="row">
-                <Show when={open()}>
-                  <Card sx={{ "height": "100vh", "width": "10vw" }}>
-                    <Sidebar />
-                  </Card>
-                </Show>
-                <Box flex={1}>
-                  {props.children}
-                </Box>
-              </Stack>
-              <Footer></Footer>
-            </ProjectProvider>
-          </UserProvider>
-        </AuthProvider>
+                </Stack>
+                <Footer></Footer>
+              </ProjectProvider>
+            </UserProvider>
+          </AuthProvider>
+        </LayoutProvider>
       </ThemeProvider>
     </>
   );
