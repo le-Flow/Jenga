93ee9da84a6617b6bd06a21323b75af864748522
Jonas Immanuel Lehr <privat@jonas-lehr.de>
2026-02-19 17:49:48 +0100

    package.json and pwa

--- frontend/jenga/vite.config.ts
+++ frontend/jenga/vite.config.ts
@@ -2,9 +2,34 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import suidPlugin from '@suid/vite-plugin';
 import devtools from 'solid-devtools/vite';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [devtools(), solidPlugin(), suidPlugin()],
+  plugins: [devtools(), solidPlugin(), suidPlugin(),
+  VitePWA({
+    devOptions: {
+      enabled: true,
+    },
+    manifest: {
+      name: 'Jenga',
+      short_name: 'Jenga',
+      description: 'My Awesome App description',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: '/icons/icon-192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/icon-512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ]
+    }
+  })
+  ],
   server: {
     port: 7000,
     proxy: {
