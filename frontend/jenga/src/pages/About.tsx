14dff03f674f428e657428743bdb99011427a0c5
Jonas Immanuel Lehr <privat@jonas-lehr.de>
2026-02-23 03:19:18 +0100

    About Page

--- frontend/jenga/src/pages/About.tsx
+++ frontend/jenga/src/pages/About.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardHeader } from "@suid/material"
+import { Card, CardContent, CardHeader, Link, Stack, Typography } from "@suid/material"
 import { useContext } from "solid-js"
 import { I18nContext } from "../provider/I18nProvider"
 
@@ -8,7 +8,20 @@ export const About = () => {
     return (
         <Card>
             <CardHeader title={i18n?.t("pages.about.title")}></CardHeader>
-            <CardContent></CardContent>
+            <CardContent>
+                <Stack spacing={2}>
+                    <Typography variant="body1">{i18n?.t("pages.about.description")}</Typography>
+                    <Typography variant="body2" color="text.secondary">{i18n?.t("pages.about.earlyDevelopment")}</Typography>
+                    <Stack direction="row" spacing={2} flexWrap="wrap">
+                        <Link href="https://github.com/Jenga-PMS/Jenga" target="_blank" rel="noreferrer">
+                            {i18n?.t("pages.about.links.sourceCode")}
+                        </Link>
+                        <Link href="https://jenga-pms.github.io/" target="_blank" rel="noreferrer">
+                            {i18n?.t("pages.about.links.documentation")}
+                        </Link>
+                    </Stack>
+                </Stack>
+            </CardContent>
         </Card>
     )
 }
